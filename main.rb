fc416102b05b3845a6bfa980f4f51913b511caee
Luke Jeremy <luke.jeremy@me.com>
2024-02-17 05:53:09 +1000

    Add helper class

--- main.rb
+++ main.rb
@@ -1,3 +1,13 @@
 class Main
+  def hello
+    Helpers.say_hello
+  end
 
+  class Helpers
+    class << self
+      def say_hello
+        "hello"
+      end
+    end
+  end
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe Main do
     it "exists" do
       expect(described_class).to_not be_nil
     end
+
+    it "has an instance method" do
+      expect(described_class.new.hello).to eq 'hello'
+    end
+
+    it "has a static helper" do
+      expect(described_class::Helpers.say_hello).to eq 'hello'
+    end
   end
 end
