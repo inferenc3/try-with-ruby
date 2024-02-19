e708f10327bfeb1f0f6aa14c5c2d50f98e26aa6c
Luke Jeremy <luke.jeremy@me.com>
2024-02-19 12:59:34 +1000

    Configure interactive debugging and LSP

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,20 +1,20 @@
-require './spec_helper.rb'
-require './main.rb'
+require "./spec_helper.rb"
+require "./main.rb"
 
 RSpec.describe Main do
   context "Setup" do
     it "exists" do
-      expect(described_class).to_not be_nil
+      expect(described_class).to be_nil
     end
 
     it "has an instance method" do
-      expect(described_class.new.hello).to eq 'hello'
+      expect(described_class.new.hello).to eq "hello"
     end
   end
 
   context "Helpers" do
     it "has a static helper" do
-      expect(described_class::Helpers.say_hello).to eq 'hello'
+      expect(described_class::Helpers.say_hello).to eq "hello"
     end
   end
 end
