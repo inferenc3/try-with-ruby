af526e671070b34124e33af6a097b55f61fcc1b4
Luke Jeremy <luke.jeremy@me.com>
2024-02-19 21:18:22 +1000

    checkpoint

--- main.rb
+++ main.rb
@@ -1,3 +1,88 @@
+# know
+# - 10x10 board on which ships can be placed
+# - ships can be vertical or horizontal lines of width 1
+# - ships must fit on board and cannot overlap
+# - a ship is sunk if all its points have been hit
+# - battleships are present at beginning of game
+# - a game has two boards (one per player)
+# - ships cannot overlap:
+# - - all ship points are unique
+# - - aha can easily lookup ship by point
+
+# maybe - hash of points to ships
+# - each ship could have point collection used for lookup when firing
+# - hash of points -> ship enables quick firing
+# - ship itself keeps of track of firing
+# - ship can indicate when it is sunk
+# { Point(x: 1, y: 1) => ship_1 }
+# hash[Point.new(x: 1, y: 1)]&.fire(1, 1) ? 'hit' : 'miss'
+# reflect - data structure idea emerged from investigation, not plucked.
+# know - this allows quick firing, 
+# win detection would be iterating over ships and checking sunk?
+# adding a ship is fairly easy:
+# - convert to point collection and update hash
+# - easy overlap detection this way too!
+
+# introduce: example session
+# board = Board.new(width: 10, height: 10)
+# board.add_ships([ship_1, ship_2])
+# board.fire(x: 1, y: 1) --> boolean
+# board.all_battleships_sunk? --> boolean
+# board is responsible for:
+# - win detection (check if all are sunk)
+# - overlap prevention (check all points unique)
+# - check all ships are within board (coordinate validation)
+# - delegating firing (lookup ship by coordinate)
+
+# introduce: ship model
+# ship_1 = BattleShip.new(x: 1, y: 1, length: 5, orientation: 'horizontal')
+# ship_2 = BattleShip.new(x: 1, y: 2, length: 2, orientation: 'vertical')
+# ship_1_points = ship_1.to_points -> [Point.new(x: 1, y:1), ...]
+# ship_1.fire(1, 1) --> boolean
+# ship_1.sunk? --> boolean
+# ship is responsible for:
+# - generating point collection
+# - tracking fire hits
+# - checking if sunk
+
+# reflect - uncertainty growing, progress slowing, 
+# perhaps try something to learn more
+
+BattleShip = Data.define(:x, :y, :length, :orientation) do
+  def initialize(x:, y:, length:, orientation:)
+    @points = {}
+
+    if orientation == 'horizontal'
+      for x in (x..(x + length))
+        @points[[x, y]] = false
+      end
+    else
+      for y in (y..(y + length))
+        @points[[x, y]] = false
+      end
+    end
+
+    super(x: x, y: x, length: length, orientation: orientation)
+  end
+
+  def fire(x:, y:)
+    if @points[[x, y]].nil?
+      false
+    else
+      @points[[x, y]] = true
+      true
+    end
+  end
+
+  def sunk?
+    @points.values.all? { |v| v == true }
+  end
+
+  def to_points
+    @points.keys.clone
+  end
+end
+
 class Main
   def hello
     H.say_hello
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -22,3 +22,51 @@ RSpec.describe Main do
     end
   end
 end
+
+RSpec.describe BattleShip do
+  let(:horizontal_ship) do
+    BattleShip.new(
+      x: 0,
+      y: 0,
+      length: 3,
+      orientation: "horizontal",
+    )
+  end
+
+  let(:vertical_ship) do
+    BattleShip.new(
+      x: 3,
+      y: 6,
+      length: 2,
+      orientation: "vertical",
+    )
+  end
+
+  it "reports hit or miss on horizontal ship" do
+    expect(horizontal_ship.fire(x: 1, y: 0)).to eq true
+    expect(horizontal_ship.fire(x: 1, y: 4)).to eq false
+  end
+
+  it "reports hit or miss on horizontal ship" do
+    expect(vertical_ship.fire(x: 3, y: 6)).to eq true
+    expect(vertical_ship.fire(x: 1, y: 1)).to eq false
+  end
+
+  it "reports whether sunk?" do
+    vertical_ship.fire(x: 3, y: 6)
+    vertical_ship.fire(x: 3, y: 7)
+    vertical_ship.fire(x: 3, y: 8)
+
+    expect(vertical_ship.sunk?).to eq true
+  end
+
+  fit "converts to points" do
+    expected_points = [
+      [3, 6],
+      [3, 7],
+      [3, 8],
+    ].to_set
+
+    expect(vertical_ship.to_points.to_set).to eq expected_points
+  end
+end
