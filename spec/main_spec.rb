f08dc3f7e2e9e04ef408fa29ec5e7ca12279c39b
Luke Jeremy <luke.jeremy@me.com>
2024-02-19 14:22:20 +1000

    Fix test

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,7 +4,7 @@ require "./main.rb"
 RSpec.describe Main do
   context "Setup" do
     it "exists" do
-      expect(described_class).to be_nil
+      expect(described_class).to_not be_nil
     end
 
     it "has an instance method" do
