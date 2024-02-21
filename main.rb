3fff53cb8653bb444fcb2c13e1686bf612282b3d
Luke Jeremy <luke.jeremy@me.com>
2024-02-21 10:53:33 +1000

    Remove scaffolding and use snippets instead

--- main.rb
+++ main.rb
@@ -1,17 +1,1 @@
 # Check Ruby LSP is running.
-
-class Main
-  def hello
-    H.say_hello
-  end
-
-  class Helpers
-    class << self
-      def say_hello
-        "hello"
-      end
-    end
-  end
-
-  H = Helpers
-end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,24 +1,2 @@
 require "./spec_helper.rb"
 require "./main.rb"
-
-RSpec.describe Main do
-  context "Main" do
-    let(:instance) { described_class.new }
-
-    it "exists" do
-      expect(described_class).to_not be_nil
-    end
-
-    it "has an instance method" do
-      expect(instance.hello).to eq "hello"
-    end
-  end
-
-  context "Helpers" do
-    let(:h) { described_class::H }
-
-    it "has a static helper" do
-      expect(h.say_hello).to eq "hello"
-    end
-  end
-end
