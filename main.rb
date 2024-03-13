32372e332cc5933f66199b4a666cc2452d4f11e9
Luke Jeremy <luke.jeremy@certsy.com>
2024-03-13 16:20:32 +1000

    Draft

--- main.rb
+++ main.rb
@@ -1,1 +1,44 @@
 # Check Ruby LSP is running.
+
+class RowCreator
+  def initialize(num_rows, station_names_and_averages)
+    @num_rows = num_rows
+    @station_names_and_averages = station_names_and_averages
+  end
+
+  def generate
+    @station_names_and_averages.cycle.map do |station_name, average|
+      yield "#{station_name};#{average}"
+    end
+  end
+
+  def generate_finite_sequence(limit)
+    counter = 0
+
+    generate do |row|
+      yield row
+      counter = counter + 1
+      break if counter == limit
+    end
+  end
+
+  def dump(path)
+    File.open(path, 'a') do |file|
+      file.puts "This is the line I want to append."
+    end
+  end
+end
+
+class Helpers
+  class << self
+    def write_lines_to_file(path, lines)
+      File.open(path, 'a') do |file|
+        for line in lines 
+          file.puts line 
+        end
+      end       
+    end
+  end
+end
+
+H = Helpers
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,2 +1,62 @@
 require "./spec_helper.rb"
 require "./main.rb"
+
+RSpec.describe RowCreator do
+  let(:num_rows) { 3 }
+
+  let(:station_names_and_averages) do
+    [
+      ["station1", "1"], 
+      ["station2", "2"], 
+      ["station3", "3"],
+    ]
+  end
+
+  let(:instance) { described_class.new(num_rows, station_names_and_averages) }
+
+  it 'generates rows' do
+    limit = 15
+    counter = 0
+
+    instance.generate do |row|
+      parsed_row = row.split(';')
+
+      expect(parsed_row).to eq station_names_and_averages[counter % 3] 
+      
+      counter = counter + 1
+      break if counter == limit
+    end
+  end
+
+  it 'generates a finite sequence' do
+    limit = 15
+    counter = 0
+
+    instance.generate_finite_sequence(9) do |row|
+      parsed_row = row.split(';')
+
+      counter = counter + 1
+      break if counter == limit
+    end
+
+    expect(counter).to eq 9 
+  end
+end
+
+RSpec.describe Helpers do
+  context '#write_lines_to_file' do
+    let(:path) { "test_file.txt" }
+
+    before do
+      File.delete(path) if File.exist?(path)
+    end
+
+    it 'writes lines' do
+      lines = ["line1", "line2", "line3"]
+
+      described_class.write_lines_to_file(path, lines)
+
+      expect(File.read(path)).to eq (lines.join("\n") + "\n")
+    end
+  end
+end
